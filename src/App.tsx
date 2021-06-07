1251f02b695495f61a385255d0028252aa0e2d04
Nguyen Quoc <quocnguyen612k@gmail.com>
2021-06-07 12:21:56 +0700

    add real-time detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import dependencies
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import Webcam from "react-webcam";
+import "./App.css";
+import { nextFrame } from "@tensorflow/tfjs";
+import { drawRect } from "./utilities";
 
 function App() {
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const runCoco = async () => {
+    const net = await tf.loadGraphModel(
+      "https://tsjs-real-time-model.s3.jp-tok.cloud-object-storage.appdomain.cloud/undefinedmodel.json"
+    );
+
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 16.7);
+  };
+
+  const detect = async (net: any) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video!.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video!.videoWidth;
+      const videoHeight = webcamRef.current.video!.videoHeight;
+
+      // Set video width
+      webcamRef.current.video!.width = videoWidth;
+      webcamRef.current.video!.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current!.width = videoWidth;
+      canvasRef.current!.height = videoHeight;
+
+      // 4. TODO - Make Detections
+      const img = tf.browser.fromPixels(video!);
+      const resized = tf.image.resizeBilinear(img, [640, 480]);
+      const casted = resized.cast("int32");
+      const expanded = casted.expandDims(0);
+      const obj = await net.executeAsync(expanded);
+      console.log(obj);
+
+      const boxes = await obj[1].array();
+      const classes = await obj[2].array();
+      const scores = await obj[4].array();
+
+      canvasRef.current!.width = videoWidth;
+      canvasRef.current!.height = videoHeight;
+      // Draw mesh
+      const ctx = canvasRef.current!.getContext(
+        "2d"
+      ) as CanvasRenderingContext2D;
+
+      // 5. TODO - Update drawing utility
+      // drawSomething(obj, ctx)
+      requestAnimationFrame(() => {
+        drawRect(
+          boxes[0],
+          classes[0],
+          scores[0],
+          0.8,
+          videoWidth,
+          videoHeight,
+          ctx
+        );
+      });
+
+      tf.dispose(img);
+      tf.dispose(resized);
+      tf.dispose(casted);
+      tf.dispose(expanded);
+      tf.dispose(obj);
+    }
+  };
+
+  useEffect(() => {
+    runCoco();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+          ref={webcamRef}
+          muted={true}
+          style={{
+            //display: "none",
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+          }}
+        />
       </header>
     </div>
   );
